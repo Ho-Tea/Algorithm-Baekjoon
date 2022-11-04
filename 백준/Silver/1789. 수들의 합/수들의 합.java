0e8d3001d26c4e9b91445327022804c637eab284
YoonJuHo <dbswn990@naver.com>
2022-11-05 02:07:00 +0900

    [Silver V] Title: 수들의 합, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer((br.readLine()));
+		long num = Long.parseLong(st.nextToken());
+		long sum = 0;
+		int count = 0;
+		for(int i = 1; ;i++){
+			if(sum > num)
+				break;
+			sum += i;
+			count++;
+		}
+		System.out.println(count-1);
+			}
+
+}
