35f2c7e4d37388132308f709497a0e655307075f
YoonJuHo <dbswn990@naver.com>
2025-01-05 15:46:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 304 ms, Memory: 23440 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,74 +1,71 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-  static int [][] map;
-  static int [][] map2;
-  static boolean [] visit;
-  static boolean [] visit2;
-  static List<Integer> chain;
-  static List<Integer> chain2;
-  static Queue<Integer> q;
-  static int N;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
-    int V = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
 
-    map = new int[N+1][N+1];
-    visit = new boolean[N+1];
-    map2 = new int[N+1][N+1];
-    visit2 = new boolean[N+1];
+        int[][] matrix = new int[n][n];
+        List<Integer> dfsResult = new ArrayList<>();
+        boolean[] dfsVisited = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
+            int node1 = Integer.parseInt(stringTokenizer.nextToken());
+            int node2 = Integer.parseInt(stringTokenizer.nextToken());
 
-    chain = new ArrayList<>();
-    chain2 = new ArrayList<>();
-    q = new LinkedList<>();
+            matrix[node1 - 1][node2 - 1] = 1;
+            matrix[node2 - 1][node1 - 1] = 1;
+        }
 
-    for(int i = 0; i < M; i++){
-      StringTokenizer st1 = new StringTokenizer(br.readLine());
-      int firstNode = Integer.parseInt(st1.nextToken());
-      int secondNode = Integer.parseInt(st1.nextToken());
+        dfs(dfsResult, matrix, v-1, dfsVisited);
+        for (int i = 0; i < dfsResult.size(); i++) {
+            System.out.print(dfsResult.get(i) + 1 + " ");
+        }
 
-      map[firstNode][secondNode] = 1;
-      map2[firstNode][secondNode] = 1;
+        System.out.println();
+        List<Integer> bfsResult = new ArrayList<>();
+        boolean[] bfsVisited = new boolean[n];
+        bfs(bfsResult, matrix, v-1, bfsVisited);
+        for (int i = 0; i < bfsResult.size(); i++) {
+            System.out.print(bfsResult.get(i) + 1 + " ");
+        }
     }
-    dfs(V);
-    for(int node : chain){
-      System.out.print(node + " ");
-    }
-    System.out.println();
 
-    bfs(V);
-    for(int node : chain2){
-      System.out.print(node+ " ");
+    public static void dfs(List<Integer> result, int[][] matrix, int node, boolean[] visited) {
+        result.add(node);
+        visited[node] = true;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[node][i] == 1 && !visited[i]) {
+                dfs(result, matrix, i, visited);
+            }
+        }
     }
-    
-  }
 
-  public static void dfs(int i){
-    visit[i] = true;
-    chain.add(i);
-    for(int j = 1;j<N+1;j++){
-      if((map[i][j] == 1 || map[j][i] == 1) && visit[j] == false){
-        dfs(j);
-      }
+    public static void bfs(List<Integer> result, int[][] matrix, int node, boolean[] visited){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        visited[node] = true;
+        while (!queue.isEmpty()) {
+            int curNode = queue.poll();
+            result.add(curNode);
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[curNode][i] == 1 && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
     }
-  }
-
-  public static void bfs(int i){
-    q.offer(i);
-    visit2[i] = true;
-    while(!q.isEmpty()){
-      int node = q.poll();
-      chain2.add(node);
-      for(int j = 1; j < N +1; j ++){
-        if((map2[node][j] == 1 || map2[j][node] == 1) && visit2[j] == false){
-          q.offer(j);
-          visit2[j] = true;
-      }
-    }
-  }
-}
 }
