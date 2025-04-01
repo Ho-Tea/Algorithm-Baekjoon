373cacf925972fbc4b9043ad502f90ea4429d7f2
YoonJuHo <dbswn990@naver.com>
2025-04-01 15:13:10 +0900

    [Silver I] Title: 미로 탐색, Time: 172 ms, Memory: 16988 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,74 +1,50 @@
-import java.util.*;
-import java.io.*;;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
-public class Main{
-    static boolean [][] visited;
-    static int [][] move = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    static int [][] map;
-    static int [][] difs;
-    static List<Integer> countList = new ArrayList<>();
-    static int count;
-    static int N;
-    static int M;
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        difs = new int[N][M];
-        for(int i = 0; i < N; i++){
-            StringTokenizer st1 = new StringTokenizer(bf.readLine());
-            String[] row = st1.nextToken().split("");
-            int j = 0;
-            for(String node : row){
-                map[i][j] = Integer.parseInt(node);
-                j++;
-            }
+public class Main {
+    static int[][] move = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = array[0];
+        int M = array[1];
+        int[][] matrix = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            int[] array1 = Arrays.stream(reader.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+            matrix[i] = array1;
         }
-
-        visited = new boolean[N][M];
-        bfs(0,0);
-        
-    }
-           
-    static void dfs(int row, int column, int[][] map){
-        count++;
-        visited[row][column] = true;
-        for(int[] moved : move){
-            if(!(row+moved[0] > N-1 || row+moved[0] < 0 || column+moved[1] > M-1 || column+moved[1] < 0 )){
-                if(map[row+moved[0]][column+moved[1]] == 1 && visited[row+moved[0]][column+moved[1]] == false){
-                    dfs(row + moved[0], column + moved[1], map);
-                }
+        boolean[][] visited = new boolean[N][M];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            if (poll[0] == N - 1 && poll[1] == M - 1) {
+                System.out.println(poll[2]);
+                return;
             }
-        }
-    }
-
-    static void bfs(int row, int column){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {row, column});
-        visited[row][column] = true;
-        difs[row][column] = 1;
-        while(!q.isEmpty()){
-            int[] temp = q.poll();
-            for(int[] moved : move){
-                if(!(temp[0]+moved[0] > N-1 || temp[0] + moved[0] < 0 || temp[1]+moved[1] > M-1|| temp[1] + moved[1] <0) ){
-                    if(map[temp[0]+moved[0]][temp[1] + moved[1]] == 1 && visited[temp[0]+moved[0]][temp[1] + moved[1]] == false){
-                        visited[temp[0]+moved[0]][temp[1] + moved[1]] = true;
-                        q.offer(new int[] {temp[0]+moved[0], temp[1]+moved[1]});
-                        difs[temp[0]+moved[0]][temp[1] + moved[1]] = difs[temp[0]][temp[1]] + 1;
-                        if(temp[0] + moved[0] == N-1 && temp[1] + moved[1] == M-1 ){
-                            System.out.println(difs[temp[0]+moved[0]][temp[1] + moved[1]]);
-                            return;
-                        }
-                    }
-                    
+            int x = poll[0];
+            int y = poll[1];
+            int cost = poll[2];
+            for (int[] moved : move) {
+                int newX = x + moved[0];
+                int newY = y + moved[1];
+                if (newX >= 0 && newY >= 0 && newX < matrix.length && newY < matrix[0].length && !visited[newX][newY] && matrix[newX][newY] == 1) {
+                    queue.offer(new int[]{newX, newY, cost + 1});
+                    visited[newX][newY] = true;
                 }
             }
         }
 
-
     }
-
-    }
+}
